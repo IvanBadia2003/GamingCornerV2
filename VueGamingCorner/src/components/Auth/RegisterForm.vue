92c8c57256b903b9f3628325fcc2d242a0d92a9e
IvanBadia2003 <ibadiamiguel@gmail.com>
2025-04-26 17:01:53 +0200

    add img for the authview and finish authview

--- VueGamingCorner/src/components/Auth/RegisterForm.vue
+++ VueGamingCorner/src/components/Auth/RegisterForm.vue
@@ -1,17 +1,21 @@
 <template>
     <v-form @submit.prevent="registrar">
-      <h2 class="mb-6">Crear cuenta</h2>
+        <v-card-title class="text-center mb-6">
+            <h2 class="mb-6">Crear cuenta</h2>
+        </v-card-title>
   
-      <v-text-field label="Nombre de usuario" v-model="nombre" />
-      <v-text-field label="Correo electrónico" v-model="email" />
-      <v-text-field label="Contraseña" type="password" v-model="password" />
-  
-      <v-btn type="submit" color="primary" block class="mt-4">Registrarse</v-btn>
-  
-      <p class="text-center mt-6">
-        ¿Ya tienes cuenta?
-        <v-btn variant="text" @click="$emit('switch')">Iniciar sesión</v-btn>
-      </p>
+        <v-card-text class="text-center mb-4">
+            <v-text-field label="Nombre de usuario" v-model="nombre" />
+            <v-text-field label="Correo electrónico" v-model="email" />
+            <v-text-field label="Contraseña" type="password" v-model="password" />
+            
+            <v-btn type="submit" color="primary" block class="mt-4">Registrarse</v-btn>
+            
+            <p class="text-center mt-6">
+                ¿Ya tienes cuenta?
+                <v-btn variant="text" @click="$emit('switch')">Iniciar sesión</v-btn>
+            </p>
+        </v-card-text>
     </v-form>
   </template>
   
--- VueGamingCorner/src/views/AuthView.vue
+++ VueGamingCorner/src/views/AuthView.vue
@@ -1,64 +1,80 @@
 <template>
-    <v-container class="auth-container">
-      <div :class="['auth-card', isRegister ? 'flipped' : '']">
-        <!-- Lado login -->
-        <div class="auth-side front">
-          <LoginForm @switch="isRegister = true" />
+  <v-container class="auth-container">
+    <v-row>
+      <v-col cols="12" md="6" :class="imageDisplay">
+        <v-img src="@/../src/assets/Auth/loginIMG.png" max-width="100%" aspect-ratio="1.5" />
+      </v-col>
+
+      <v-col cols="12" md="6" class="d-flex justify-center align-center">
+        <div :class="['auth-card', isRegister ? 'flipped' : '']">
+          <!-- Lado login -->
+          <div class="auth-side front bg-primary">
+            <LoginForm @switch="isRegister = true" />
+          </div>
+          <!-- Lado registro -->
+          <div class="auth-side back bg-primary">
+            <RegisterForm @switch="isRegister = false" />
+          </div>
         </div>
-        <!-- Lado registro -->
-        <div class="auth-side back">
-          <RegisterForm @switch="isRegister = false" />
-        </div>
-      </div>
-    </v-container>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref } from 'vue'
-  import LoginForm from '@/components/Auth/LoginForm.vue'
-  import RegisterForm from '@/components/Auth/RegisterForm.vue'
-  
-  const isRegister = ref(false)
-  </script>
-  
-  <style scoped lang="scss">
-  .auth-container {
-    perspective: 1000px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    min-height: 80vh;
-  }
-  
-  .auth-card {
-    width: 400px;
-    height: 500px;
-    position: relative;
-    transform-style: preserve-3d;
-    transition: transform 0.8s;
-  }
-  
-  .auth-card.flipped {
-    transform: rotateY(180deg);
-  }
-  
-  .auth-side {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    backface-visibility: hidden;
-    background-color: white;
-    border-radius: 16px;
-    padding: 32px;
-    box-shadow: 0 20px 30px rgba(0, 0, 0, 0.3);
-  }
-  
-  .front {
-    z-index: 2;
-  }
-  
-  .back {
-    transform: rotateY(180deg);
-  }
-  </style>
-  
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import LoginForm from '@/components/Auth/LoginForm.vue'
+import RegisterForm from '@/components/Auth/RegisterForm.vue'
+import { useDisplay } from 'vuetify'
+const { xs, sm, md, lg } = useDisplay()
+
+const imageDisplay = computed(() => {
+    if (xs.value) return 'd-none'
+    if (sm.value) return 'd-none'
+    if (md.value) return 'd-flex'
+    if (lg.value) return 'd-flex'
+    return 'd-flex'
+})
+const isRegister = ref(false)
+</script>
+
+<style scoped lang="scss">
+.auth-container {
+  perspective: 1000px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 80vh;
+}
+
+.auth-card {
+  width: 400px;
+  height: 600px;
+  position: relative;
+  transform-style: preserve-3d;
+  transition: transform 0.8s;
+}
+
+.auth-card.flipped {
+  transform: rotateY(180deg);
+}
+
+.auth-side {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  background-color: white;
+  border-radius: 16px;
+  padding: 32px;
+  box-shadow: 0 20px 30px rgba(0, 0, 0, 0.3);
+}
+
+.front {
+  z-index: 2;
+}
+
+.back {
+  transform: rotateY(180deg);
+}
+</style>
